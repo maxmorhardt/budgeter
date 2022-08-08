d24ac8ce9a168e44de12ec68f41972ce70317084
Max Morhardt <maxmorhardt13@gmail.com>
2022-08-08 16:31:29 -0400

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./screens/login";
+import Signup from "./screens/signup";
+import Home from "./screens/home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/' element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
 );
 
