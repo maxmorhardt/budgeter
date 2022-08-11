b94a3d22118d79735e63b8b7350db6e35a835cbf
Max Morhardt <maxmorhardt13@gmail.com>
2022-08-11 17:20:04 -0400

    testing stuff

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 require('dotenv').config()
-
 const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/models')
 const path = require('path')
-
 const app = express()
-app.use(express.json())
 const port = process.env.PORT
-
-console.log(process.env)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+  })
   mongoose.connect(process.env.MONGODB_PROD_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 } else {
-  mongoose.connect(process.env.MONGODB_DEV_URI, {
+  mongoose.connect(process.env.MONGODB_LOCAL_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -28,7 +26,9 @@ if (process.env.NODE_ENV === 'production') {
     .catch(err => console.log(err))
 }
 
+app.use(express.json())
 app.get('/api/users', async (req, res) => {
+  console.log(process.env)
   const users = await User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
