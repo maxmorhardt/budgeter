987f0b9b4424a8296ecc694db156dc88e3cb94da
Max Morhardt <maxmorhardt13@gmail.com>
2022-08-17 21:32:26 -0400

    Added loading screen

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import SyncLoader from 'react-spinners/SyncLoader'
+
 
 const Loading = () => {
+  
   return (
-    <div className="loading">
-      <div className="loading__spinner"></div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <img 
+        src={require('../../assets/budgeter-192x192.png')} 
+        className='w-32 h-32 mb-14'
+      />
+      <SyncLoader color='#3d5f82'/>
     </div>
   )
 }
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -3,16 +3,22 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import Sidebar from '../../components/sidebar'
+import Loading from '../../components/loading'
 import toastConfigs from '../../helpers/toastConfigs'
 import { API_PATH } from '../../helpers/environ'
 
 // Home page
 const Dashboard = () => {
-  const navigate = useNavigate()
   const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
 
   // Check if token exists and is valid
   useEffect(() => {
+    // DEV TESTING WITH LOADING SCREEN
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+
     const token = localStorage.getItem('token')
     if (!token) {
       navigate('/login')
@@ -26,22 +32,27 @@ const Dashboard = () => {
           toast.error('Session expired', toastConfigs)
           localStorage.removeItem('token')
           navigate('/login')
-        })
-        .finally(() => {
-          setLoading(false)
+        }).finally(() => {
+          // Set this when data needs to be fetched
+          //setLoading(false)
         })
     }
   } , []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <div className='flex'>
-      <Sidebar />
-      <div className='flex-[6]'>
-        <h1>container</h1>
+  if (!loading) {
+    return (
+      <div className='flex'>
+        <Sidebar />
+        <div className='flex-[6]'>
+          <h1>container</h1>
+        </div>
       </div>
-    </div>
-
-  )
+    )
+  } else {
+      return (
+        <Loading />
+      )
+    }
 }
 
 export default Dashboard
